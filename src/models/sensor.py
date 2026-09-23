6cb3db4159f2ecce9dbe823ef04e283443a10d45
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-09-23 16:56:22 -0400

    Update sensor.py

--- src/models/sensor.py
+++ src/models/sensor.py
@@ -1,15 +1,19 @@
 # Camera and Lighting Positioning Calculator
 #
 # Author: Peter Jakubowski
-# Date: 2/14/2025
+# Date: 9/23/2026
 # Description:
 #
 
 from pydantic import BaseModel
 
+# ==============================
+# ======== Sensor Info =========
+# ==============================
+
 
 class Sensor(BaseModel):
-    sensor_w_mm: float
-    sensor_h_mm: float
-    sensor_w_px: int
-    sensor_h_px: int
+    sensor_w_mm: float  # width of the sensor in millimeters
+    sensor_h_mm: float  # height of the sensor in millimeters
+    sensor_w_px: int  # width of the sensor in pixels
+    sensor_h_px: int  # height of the sensor in pixels
